aad2734e24fbfbe8eb3d493b67697b3bda9d364e
BugraAslan <bugra.aslan@path.com.tr>
2020-11-14 06:26:01 +0300

    relation test

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -6,10 +6,43 @@ use App\Entity\Player;
 
 class PlayerController extends BaseController
 {
-    public function info()
+    // TODO prototype !!!
+    public function playerVillageInfo()
     {
         /** @var Player $player */
         $player = $this->getUser();
-        return $this->successResponse($player);
+
+        $villageData = [];
+        foreach ($player->getVillages() as $playerVillage){
+            $villageUnits = [];
+            foreach ($playerVillage->getVillageUnits() as $villageUnit){
+                $villageUnits = [
+                    'unit_id' => $villageUnit->getUnit()->getId(),
+                    'unit_name' => $villageUnit->getUnit()->getName(),
+                    'unit_count' => $villageUnit->getUnitCount()
+                ];
+            }
+
+            $villageBuildings = [];
+            foreach ($playerVillage->getVillageBuildings() as $villageBuilding){
+                $villageBuildings = [
+                    'building_id' => $villageBuilding->getBuilding()->getId(),
+                    'building_name' => $villageBuilding->getBuilding()->getName(),
+                    'building_level' => $villageBuilding->getBuildingLevel()
+                ];
+            }
+
+            $villageData[] = [
+                'village_id' => $playerVillage->getId(),
+                'village_score' => $playerVillage->getScore(),
+                'village_coordinate_x' => $playerVillage->getCoordinateX(),
+                'village_coordinate_y' => $playerVillage->getCoordinateY(),
+                'village_continent' => $playerVillage->getContinent(),
+                'village_units' => $villageUnits,
+                'village_buildings' => $villageBuildings
+            ];
+        }
+
+        return $this->successResponse($villageData);
     }
 }
