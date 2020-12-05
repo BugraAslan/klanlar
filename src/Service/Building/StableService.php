4bcaa41b7737e37f7deb7613a686bf6a4ab13c1c
BugraAslan <bugra.aslan@path.com.tr>
2020-12-05 20:54:44 +0300

    stable service

---
+++ src/Service/Building/StableService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service\Building;
+
+use App\Entity\VillageBuilding;
+use App\Model\Response\Building\UnitManufacturerBuildingDetailResponse;
+use App\Strategy\BuildingStrategyInterface;
+
+class StableService extends AbstractBaseBuildingService implements BuildingStrategyInterface
+{
+    public const BUILDING_NAME = 'Ahır';
+
+    /**
+     * @param string $buildingName
+     * @return bool
+     */
+    public function canHandle(string $buildingName): bool
+    {
+        return self::BUILDING_NAME === $buildingName;
+    }
+
+    /**
+     * @param VillageBuilding $villageBuilding
+     * @return UnitManufacturerBuildingDetailResponse|null
+     */
+    public function buildingDetail(VillageBuilding $villageBuilding): ?UnitManufacturerBuildingDetailResponse
+    {
+        $buildingDetail = null;
+        if ($this->isUnitManufacturer($villageBuilding)){
+            $buildingDetail = $this->getUnitManufacturerBuildingDetail($villageBuilding);
+        }
+
+        return $buildingDetail;
+    }
+}
