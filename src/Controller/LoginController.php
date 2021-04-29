aa97fb84321aaf9cef3bb63c6cf93ff1f1cf65a9
BugraAslan <bugraaslangg@gmail.com>
2021-04-29 19:57:06 +0300

    test commit

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -10,10 +10,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\ConstraintViolationList;
 
-/**
- * Class LoginController
- * @package App\Controller
- */
 class LoginController extends BaseController
 {
     /** @var LoginService */
